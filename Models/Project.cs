6b328d5fc9737d4cfdb58a6e6b50d46d3da328d8
englebt <englebt@gmail.com>
2017-11-21 16:40:48 -0500

    WIP, adding Project properties.

--- Models/Project.cs
+++ Models/Project.cs
@@ -7,8 +7,22 @@ namespace ProjectManager.Models
 {
     public class Project
     {
+        public enum Statuses
+        {
+
+        }
+
         public int Id { get; set; }
         public string Name { get; set; }
+        public string Number { get; set; }
+
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+
+        public Statuses Status { get; set; }
+
+        public double ContractValue { get; set; } 
+        public double FundingValue { get; set; }
 
         public virtual List<Employee> Employees { get; set; }
     }
